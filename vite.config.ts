916d62fb57588f4bd460fa7a941287b48f72cbc7
stillistica <rozhkovavika24@gmail.com>
2025-12-05 18:47:35 +0200

    fix bugs

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ import fullReload from "vite-plugin-full-reload";
 const pages = ["index.html", "pages/second.html"];
 
 export default defineConfig({
-  base: "./",
+  base: "/ClubTravel/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -54,6 +54,7 @@ export default defineConfig({
   ],
 
   build: {
+    
     outDir: "docs",
     emptyOutDir: true,
     rollupOptions: {
