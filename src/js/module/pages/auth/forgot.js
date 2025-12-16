e0f7ad7ab0fea84a06f96384f922fb13efa97864
Roman <romanmytsu@gmail.com>
2025-12-17 01:49:19 +0200

    tourbooking done

--- src/js/module/pages/auth/forgot.js
+++ src/js/module/pages/auth/forgot.js
@@ -2,14 +2,16 @@ import { forgotPassword } from "../../api/forgotPassword";
 
 const form = document.getElementById("forgotForm");
 
-form.addEventListener("submit", async (e) => {
-  e.preventDefault();
+if (form) {
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
 
-  try {
-    await forgotPassword(form.email.value);
-    form.reset();
-    alert("Письмо отправлено");
-  } catch (err) {
-    alert("Ошибка");
-  }
-});
+    try {
+      await forgotPassword(form.email.value);
+      form.reset();
+      alert("Письмо отправлено");
+    } catch (err) {
+      alert("Ошибка");
+    }
+  });
+}
