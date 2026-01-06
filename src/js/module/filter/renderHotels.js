5c912bdba0a63019b5a0ab9de46ae19c63694039
stillistica <rozhkovavika24@gmail.com>
2026-01-06 20:41:19 +0200

    work in  renderHotelCard

--- src/js/module/filter/renderHotels.js
+++ src/js/module/filter/renderHotels.js
@@ -18,11 +18,37 @@ export function renderHotelsList(hotels) {
   list.innerHTML = hotels.map((hotel) => renderHotelCard(hotel)).join("");
 }
 
+function getHotelMeals(options = []) {
+  if (!options.length) return "-";
+
+  const mealsMap = {
+    no_meals: "Без питания",
+    breakfast: "Завтрак",
+    breakfast_dinner: "Завтрак и ужин",
+    full_board: "Завтрак, обед, ужин",
+    all_inclusive: "Всё включено",
+    ultra_all_inclusive: "Ультра: всё включено",
+  };
+
+  const option = options.reduce((min, current) => {
+    return current.price < min.price ? current : min;
+  }, options[0]);
+
+  return mealsMap[option.meals] || "-";
+}
+
 function renderHotelCard(hotel) {
   const img = getHotelImage(hotel.image);
   const name = hotel.nameHotel;
   const destination = hotel.tour?.destination;
   const region = hotel.region;
   const category = renderCategoryStars(hotel.category);
-	
+  const meals = getHotelMeals(hotel.hotel_options);
+  const days = hotel.tour_option?.days;
+  const cate = hotel.category;
+  const description = hotel.description;
+  const minPrice = hotel.tour_option?.minPrice;
+
+  return `
+  `;
 }
