e6819c55395593ba3403b68a4ce2acef5e3f79d6
stillistica <rozhkovavika24@gmail.com>
2026-02-03 16:36:39 +0200

    update bugs

--- src/js/module/pages/home-page/hotHome.js
+++ src/js/module/pages/home-page/hotHome.js
@@ -98,7 +98,7 @@ if (containerHotPageSwiper) {
       return `
         <div class="hothome__slider-swiper-item swiper-slide">
           <div class="hothome__slider-swiper-item-one">
-            <img src="${img}" alt="${name}" decoding="async" width="380" height="250"/>
+            <img src="${img}" alt="${name}" decoding="async" fetchpriority=high width="380" height="250"/>
             <div class="hothome__slider-swiper-item-one-list">
             <div
               class="hothome__slider-swiper-item-one-list-item hothome__slider-swiper-item-one-list-date"
